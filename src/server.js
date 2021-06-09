4f15cd2b59de2d7ec0fedcd0cc655a8dc311e887
AUDREY PATTERSON <audrey.patterson31@gmail.com>
2021-06-09 13:02:00 -0700

    adding cors

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const logger = require('./middleware/logger.js');
@@ -10,6 +11,7 @@ const dogRoutes = require('./routes/dog-routes.js');
 const notFound = require('./error-handlers/404.js');
 const errorHandler = require('./error-handlers/500.js');
 
+app.use(cors());
 app.use(express.json());
 
 app.use(logger);
